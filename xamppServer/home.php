b3e8064e6f61d25a23d4942bd6fa7c9f20125a3e
Ashish Gole <32222935+ashgole@users.noreply.github.com>
2021-02-24 10:53:50 +0530

    Update home.php

--- xamppServer/home.php
+++ xamppServer/home.php
@@ -4,11 +4,11 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = ""; 
-$dbname= "databaseName";
+$dbname= "databaseName"; //#change
 $myConn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Database not found...");
 		
 // retrieve data in for loop from mysql database	 
-$q1="SELECT * from tableName";
+$q1="SELECT * from tableName"; // #change - add your table name
 $show = null; 
 $result=mysqli_query($myConn,$q1); 
 while ($row = mysqli_fetch_array($result)) 
